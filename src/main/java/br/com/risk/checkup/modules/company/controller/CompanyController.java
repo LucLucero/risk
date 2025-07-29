46cdf41a7492c79030665e01bb0339c228e2d48b
Luciano Lucero <lucianoxlucero@hotmail.com>
2025-07-29 01:37:15 -0300

    configurado app props

--- src/main/java/br/com/risk/checkup/modules/company/controller/CompanyController.java
+++ src/main/java/br/com/risk/checkup/modules/company/controller/CompanyController.java
@@ -2,7 +2,10 @@ package br.com.risk.checkup.modules.company.controller;
 
 
 import br.com.risk.checkup.modules.company.CompanyEntity;
+import br.com.risk.checkup.modules.company.services.RegisterCompanyService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/company")
 public class CompanyController {
 
+    @Autowired
+    private RegisterCompanyService registerCompanyService;
 
 
     @PostMapping("/")
     public ResponseEntity<Object> registerCompany(@Valid @RequestBody CompanyEntity companyEntity){
+        try {
 
+            var result = registerCompanyService.execute(companyEntity);
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
 
 
     }
